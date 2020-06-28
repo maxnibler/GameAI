9bf4ffb55a4b457a39bd52fa10a7bf5fb0223960
Max <max.nibler@gmail.com>
2020-06-27 19:52:03 -0700

    check boxes for points

--- P2/p2_pathfinder.py
+++ P2/p2_pathfinder.py
@@ -1,3 +1,15 @@
+def inBox(box, coor):
+    if box[0] > coor[0]:
+        return 0
+    if box[1] < coor[0]:
+        return 0
+    if box[2] > coor[1]:
+        return 0
+    if box[3] < coor[1]:
+        return 0
+    return 1
+    
+
 def find_path (source_point, destination_point, mesh):
     """    Searches for a path from source_point to destination_point through the mesh
     Args:
@@ -8,10 +20,16 @@ def find_path (source_point, destination_point, mesh):
         A path (list of points) from source_point to destination_point if exists
         A list of boxes explored by the algorithm
     """
-    print (source_point)
-    print (destination_point)
-    print (mesh[source_point])
-   # print (mesh)
+    #print (source_point)
+    #print (destination_point)
+    #print (mesh[source_point])
+    for i in range(0,len(mesh['boxes'])):
+        cat = mesh['boxes'][i]
+        if inBox(cat,source_point):
+            scBox = cat
+        if inBox(cat, destination_point):
+            dstBox = cat
     path = []
-    boxes = {}
+    boxes = {scBox,dstBox}
+    print(boxes)
     return path, boxes.keys()
